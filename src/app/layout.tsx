0ebfea086d1c8598169a5f9573f8a7957bd2e3cf
bonyoungkoo <gby1202@gmail.com>
2025-07-07 02:46:57 +0900

    feat: layout 컴포넌트에 manifest link 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,11 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#FFC107" />
+        <link rel="icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link
           rel="stylesheet"
           as="style"
