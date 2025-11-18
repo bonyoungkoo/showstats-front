18eb5c77b3d0a0404d297655009b61d4bdb625e2
bonyoungkoo <gby1202@gmail.com>
2025-11-19 02:00:04 +0900

    feat: google search console용 sitemap 및 robots 설정 추가

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://showstats.dugout.dev/sitemap.xml",
+  };
+}
